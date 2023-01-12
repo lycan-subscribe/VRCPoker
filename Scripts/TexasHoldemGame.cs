68432d823359d868fb1327dd26c5345730d3e359
Lycan <mwiloveicecream565@gmail.com>
2023-01-11 17:34:53 -0700

    Initial commit, trying out a general structure

---
+++ Scripts/TexasHoldemGame.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VRCPoker{
+	
+	public class TexasHoldemGame : UdonSharpBehaviour
+	{
+		
+		
+		void Start()
+		{
+			
+		}
+	}
+
+}
