718eabf3cae8573b9f6b4cf88d383a656e3a6702
icedog225 <icedog225@gmail.com>
2023-02-27 09:53:51 -0800

    Created tests and VRCPoker assembly definition

---
+++ Tests/PokerHandEvaluationTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using VRCPoker;
+
+namespace Tests
+{
+    public class PokerHandEvaluationTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PokerHandEvaluationTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.True(1 == 2);
+        }
+
+        [Test]
+        public void DisableCardHand()
+        {
+            CardHand hand = new GameObject().AddComponent<CardHand>();
+        }
+    }
+}
