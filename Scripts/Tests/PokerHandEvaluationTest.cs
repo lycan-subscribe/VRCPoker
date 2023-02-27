16fcbbc1eba131a5417d5953fe9d60bf0d04dd9e
icedog225 <icedog225@gmail.com>
2023-02-27 11:28:40 -0800

    Added test for only one player

--- Scripts/Tests/PokerHandEvaluationTest.cs
+++ Scripts/Tests/PokerHandEvaluationTest.cs
@@ -7,30 +7,45 @@ using VRCPoker;
 
 namespace Tests
 {
+    [TestFixture]
     public class PokerHandEvaluationTest
     {
-        [Test]
-        public void HighCardWins()
+        private CardHand common, p0, p1, p2;
+
+        [SetUp]
+        public void CardHands()
         {
-            CardHand common = new GameObject().AddComponent<CardHand>();
+            common = new GameObject().AddComponent<CardHand>();
             common.cardRanks = new Rank[] { Rank.Seven, Rank.Six, Rank.Three, Rank.Queen, Rank.Two };
             common.cardSuits = new Suit[] { Suit.Spades, Suit.Clubs, Suit.Diamonds, Suit.Clubs, Suit.Diamonds };
 
-            CardHand p0 = new GameObject().AddComponent<CardHand>();
+            p0 = new GameObject().AddComponent<CardHand>();
             p0.cardRanks = new Rank[] { Rank.Five, Rank.Nine };
             p0.cardSuits = new Suit[] { Suit.Clubs, Suit.Hearts };
 
-            CardHand p1 = new GameObject().AddComponent<CardHand>();
+            p1 = new GameObject().AddComponent<CardHand>();
             p1.cardRanks = new Rank[] { Rank.King, Rank.Eight };
             p1.cardSuits = new Suit[] { Suit.Clubs, Suit.Hearts };
 
-            CardHand p2 = new GameObject().AddComponent<CardHand>();
+            p2 = new GameObject().AddComponent<CardHand>();
             p2.cardRanks = new Rank[] { Rank.Eight, Rank.Jack };
             p2.cardSuits = new Suit[] { Suit.Clubs, Suit.Hearts };
+        }
 
+        [Test]
+        public void HighCardWins()
+        {
             int[] winningHands = WinningHandSolver.GetWinningHands(common, new CardHand[] { p0, p1, p2 });
-            Assert.That(winningHands.Length == 1); // Exactly one winner
-            Assert.That(winningHands[0] == 1); // Player 2 (index 1) wins
+            Assert.That(winningHands.Length == 1);
+            Assert.That(winningHands[0] == 1);
+        }
+
+        [Test]
+        public void OnlyOnePlayer()
+        {
+            int[] winningHands = WinningHandSolver.GetWinningHands(common, new CardHand[] { p0 });
+            Assert.That(winningHands.Length == 1);
+            Assert.That(winningHands[0] == 0);
         }
     }
 
